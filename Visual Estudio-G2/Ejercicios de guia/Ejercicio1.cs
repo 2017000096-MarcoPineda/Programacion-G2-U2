35b02c97de7d3ffd8a2cc6175f1cdf2fc0a347bb
2017000096-MarcoPineda <m.pineda.5bdb@gmail.com>
2026-04-16 19:23:48 -0600

    Add files via upload

---
+++ Visual Estudio-G2/Ejercicios de guia/Ejercicio1.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("CALCULADORA FUNCIONES TRIGONOMETRICAS BASICAS");
+        Console.WriteLine("*********************************************");
+        Console.WriteLine("1- Seno");
+        Console.WriteLine("2- Coseno");
+        Console.WriteLine("3- Tangente");
+        Console.WriteLine("*********************************************");
+
+        Console.Write("Seleccione una opción (1-3): ");
+        int op = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ingrese el ángulo en grados: ");
+        double ang = Convert.ToDouble(Console.ReadLine());
+        double rads = ang * (Math.PI / 180);
+
+        switch (op)
+        {
+            case 1:
+                Console.WriteLine("Seno: " + Math.Sin(rads));
+                break;
+            case 2:
+                Console.WriteLine("Coseno: " + Math.Cos(rads));
+                break;
+            case 3:
+                Console.WriteLine("Tangente: " + Math.Tan(rads));
+                break;
+            default:
+                Console.WriteLine("ERROR");
+                break;
+            }   
+        }
+}
---
+++ Visual Estudio-G2/Ejercicios de guia/Ejercicio2.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Ingrese un carácter: ");
+        char c = Convert.ToChar(Console.ReadLine().ToLower());
+
+        switch (c)
+        {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                Console.WriteLine("es una vocal");
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                Console.WriteLine("es un digito");
+                break;
+            default:
+                Console.WriteLine("no es ni vocal ni digito");
+                break;
+        }
+    }
+}
